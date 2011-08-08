4865ff25858db96c823f1cfbb60fba8dadcee387
Christoph Kulla <christophkulla@googlemail.com>
2011-08-08 21:41:09 +0200

    First shot on Workspace and WorkbenchUtil rules.

--- eclipse/ui.test/src/org/junit/contrib/eclipse/ui/PluginTest.java
+++ eclipse/ui.test/src/org/junit/contrib/eclipse/ui/PluginTest.java
@@ -1,8 +1,15 @@
 package org.junit.contrib.eclipse.ui;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 public class PluginTest {
+
+	@Rule
+	public WorkbenchUtil workbenchUtil = new WorkbenchUtil ();
+
+	@Rule
+	public WorkspaceUtil workspaceUtil = new WorkspaceUtil ();
 
 	@Test
 	public void test() {
--- eclipse/ui/src/org/junit/contrib/eclipse/ui/MyClass.java
+++
@@ -1,4 +0,0 @@
-package org.junit.contrib.eclipse.ui;
-
-public class MyClass {
-}
