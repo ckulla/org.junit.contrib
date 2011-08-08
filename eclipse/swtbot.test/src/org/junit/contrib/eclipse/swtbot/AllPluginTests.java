651cc71f4e2dd56ca5fbc4fd5f4726a1ab2e0eeb
Christoph Kulla <christophkulla@googlemail.com>
2011-08-08 14:31:01 +0200

    Added CaptureScreenshotOnFailure rule.

--- eclipse/swtbot.test/src/org/junit/contrib/eclipse/swtbot/AllPluginTests.java
+++ eclipse/swtbot.test/src/org/junit/contrib/eclipse/swtbot/AllPluginTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PluginTest.class })
+@SuiteClasses({ ScreenshotTest.class })
 public class AllPluginTests {
 
 }
--- eclipse/swtbot/src/org/junit/contrib/eclipse/swtbot/MyClass.java
+++
@@ -1,4 +0,0 @@
-package org.junit.contrib.eclipse.swtbot;
-
-public class MyClass {
-}
