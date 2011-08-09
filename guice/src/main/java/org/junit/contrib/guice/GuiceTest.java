92d6ae29eeaa4fddff583a82fdb8380ea5e31a6e
Christoph Kulla <christophkulla@googlemail.com>
2011-08-09 11:49:56 +0200

    Moved eclipse projects to their own repository.

--- guice/src/main/java/org/junit/contrib/guice/GuiceTest.java
+++ guice/src/main/java/org/junit/contrib/guice/GuiceTest.java
@@ -2,10 +2,10 @@ package org.junit.contrib.guice;
 
 import org.junit.Assert;
 import org.junit.Rule;
-import org.junit.runner.RunWith;
 
 public abstract class GuiceTest extends Assert {
 
 	@Rule
 	public GuiceRule guiceRule = new GuiceRule ();
+
 }
